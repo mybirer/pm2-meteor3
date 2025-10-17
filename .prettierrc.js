f775cf0e4d69843c553cc503d604a6d141012acd
M. Yasin Birer <mybirer@gmail.com>
2025-10-17 19:39:20 +0300

    prettier configurated

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	trailingComma: 'all',
+	tabWidth: 4,
+	useTabs: true,
+	printWidth: 140,
+	semi: true,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	bracketSpacing: true,
+	bracketSameLine: true,
+	arrowParens: 'avoid',
+	singleAttributePerLine: false,
+};
